d7a429bf452b742abf7d123d91c05a371b4195f7
Sunjingwen <453129077@qq.com>
2017-12-28 21:22:41 +0800

    第二阶段

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
 import logo from './logo.svg'
-import './App.css';
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React 小美妞😘</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">React 小美妞 <span role="img" aria-label="mmd">😘</span></h1>
+				</header>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+			</div>
+		)
+	}
 }
 
-
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import CommentApp from './CommentApp'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+	<div>
+		<App />
+		<CommentApp />
+	</div>,
+	document.getElementById('root'))
 registerServiceWorker()
